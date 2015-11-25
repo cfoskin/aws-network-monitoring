41fd0eeb5cffa7c4ebb92a621600e9717fc2d338
cfoskin <columfoskin@gmail.com>
2015-11-25 11:52:06 +0000

    copy cpu100.sh and test policies

--- manage_autoscale.py
+++ manage_autoscale.py
@@ -1,11 +1,15 @@
 #colum foskin 20062042
 #!/usr/bin/python3
+# This python module provides functionality for the options in the autoscaling menus and sub menus
+# in the menu module. It provides functionality for creating autoscaling alarms and policies while also allows
+# the user to test the scaling policies.
+
 import boto.ec2.autoscale
 from boto.ec2.autoscale import ScalingPolicy
 from boto.sns import connect_to_region
 from boto.ec2.cloudwatch import connect_to_region
-from boto.ec2.elb import ELBConnection
 from boto.ec2.cloudwatch import MetricAlarm
+from utility import *
 scale_up_policy = None
 scale_down_policy = None
 autoscale_conn = None
@@ -13,13 +17,12 @@ cloudwatch_conn = None
 alarm_dimensions = None
 alarm_tag = 'cFoskin_'
 
-
+#open autoscale connection
 def open_autoscale_conn():
  global autoscale_conn
  autoscale_conn = boto.ec2.autoscale.connect_to_region('eu-west-1')  
- # asGroups = autoscale_conn.get_all_groups(names=['cFoskin_aGroup'])
- # myASGroup = asGroups[0]
- 
+
+#allows the user to create a policy to scale up
 def create_scale_up_policy():
  name = input('Enter The Name of Your new policy.. \n\n ')
  global scale_up_policy
@@ -28,8 +31,10 @@ def create_scale_up_policy():
  policyResults = autoscale_conn.get_all_policies(as_group='cFoskin_aGroup', policy_names=[scale_up_policy])
  scale_up_policy = policyResults[0]#Refresh to get Amazon Resource Name (ARN) of policy
  print('The policy : %s is now created....\n' % name)
+ print('Please create the corresponding alarm')
  return bool(1)
 
+#allows the user to create a policy to scale down
 def create_scale_downPolicy():
  name = input('Enter The Name of Your new policy.. \n\n ')
  global scale_down_policy
@@ -40,20 +45,45 @@ def create_scale_downPolicy():
  print('The policy : %s is now created....\n' % name)
  return bool(1)
 
+#allows the user to create an alarm for the scale up policy 
 def create_scale_up_alarm():
  name = input('Enter The Name of Your new Alarm.. \n\n ')
  scale_up_alarm_name= alarm_tag + name
- scale_up_alarm = MetricAlarm(name=scale_up_alarm_name, namespace='AWS/EC2',metric='CPUUtilization', statistic='Average',comparison='>', threshold='70', period='60', evaluation_periods=2,alarm_actions=[scale_up_policy.policy_arn],dimensions=alarm_dimensions)
+ scale_up_alarm = MetricAlarm(name=scale_up_alarm_name, namespace='AWS/EC2',metric='CPUUtilization', statistic='Average',comparison='>', threshold='40', period='60', evaluation_periods=2,alarm_actions=[scale_up_policy.policy_arn],dimensions=alarm_dimensions)
  cloudwatch_conn.create_alarm(scale_up_alarm)
  print('The cloud watch alarm : %s is now created....\n' % scale_up_alarm_name)
+ return bool(1)
 
+#allows the user to create an alarm for the scale down policy 
 def create_scale_down_alarm():
  name = input('Enter The Name of Your new Alarm.. \n\n ') 
  scale_down_alarm_name = alarm_tag + name
- scale_down_alarm = MetricAlarm(name=scale_down_alarm_name, namespace='AWS/EC2',metric='CPUUtilization', statistic='Average',comparison='>', threshold='70', period='60', evaluation_periods=2,alarm_actions=[scale_down_policy.policy_arn],dimensions=alarm_dimensions)
+ scale_down_alarm = MetricAlarm(name=scale_down_alarm_name, namespace='AWS/EC2',metric='CPUUtilization', statistic='Average',comparison='<', threshold='35', period='60', evaluation_periods=2,alarm_actions=[scale_down_policy.policy_arn],dimensions=alarm_dimensions)
  cloudwatch_conn.create_alarm(scale_down_alarm)
  print('The cloud watch alarm : %s is now created....\n' % scale_down_alarm_name)
+ return bool(1)
 
+#retrieves an instance and copys up the cpu100.sh using scp, make it executable and run it. 
+#user can press ctr+c at any time to exit the process and it will return to the menu.
+def trigger_autoscaling_event():
+ inst = myInstances[1]
+ ip_address = inst.ip_address
+ cmd_scp = "scp -o StrictHostKeyChecking=no -i" + key + " cpu100.sh " + "ec2-user@"+ip_address +":."  
+ if run_command(cmd_scp):
+     print('Script copied up. Making executable....')
+     cmd_exec = "ssh -t -i " + key + " " + "ec2-user@"+ip_address +" 'chmod 700 cpu100.sh'"
+     if run_command(cmd_exec):
+         cmd_run = "ssh -t -i " + key + " " + "ec2-user@"+ip_address +" './cpu100.sh'"
+         print('Process starting on instance: %s .... \n' % inst)
+         print('Press ctrl + c to kill this process and return to the main menu to view any new instances')
+         if run_command(cmd_run):
+             print('Please return to main menu now to view your instances\n')
+             print('Any new instances spun up will scale down automatically when allowed the time to do so\n')
+         else: print('Exiting ..... ')  
+     else: print('error making executable... Please retry')
+ else: print('error with scp... Please retry')
+       
+#open cloud watch connection and set dimensions
 def set_up_cloud_watch():
  global cloudwatch_conn
  cloudwatch_conn = boto.ec2.cloudwatch.connect_to_region('eu-west-1')
--- menu.py
+++ menu.py
@@ -2,34 +2,40 @@
 #!/usr/bin/python3
 #A set of menus for the user
 from termcolor import colored
-from monitor_instances import *
+from utility import *
 from manage_autoscale import *
 import sys
 connection = None
 scaling_up_policy_created = bool(0)
+scaling_up_alarm_created = bool(0)
 scaling_down_policy_created = bool(0)
+scaling_down_alarm_created = bool(0)
 
+#main menu
 def mainOptions():
+ getAllMyInstances(connection)#gather my instances and store them in an array
  choice = None
  while choice != '0':
      print(colored("\n\n Welcome to the Main Menu  ", 'green',attrs=['reverse', 'blink']))
      print('=============                 Main Menu              =============')
      print('==================================================================')
      print('|  1: View all instances in my Autoscale group                     |')
-     print('|  2: View options for Elb and Instance Access log management      |')
-     print('|  3: Manage Autoscale Group                                       |')
+     print('|  2: Manage Autoscale Group                                       |')
+     print('|  3: View options for Elb and Instance Access log management      |')              
      print(colored('|  0: EXIT                                                        |', 'red'))
      print('==================================================================')
      choice = input('which task do you want to perform? \n\n ')
      if choice == '1': listMyInstances()
-     if choice == '2': elb_options()
-     if choice == '3': autoscaleOptions()
+     if choice == '2': autoscaleOptions()
+     if choice == '3': utility_options()
      if choice == '0': sys.exit(0)  
 
-def elb_options():
+#utility menu
+def utility_options():
+ getAllMyInstances(connection)#gather my instances and store them in an array
  choice = None
  while choice != '0':
-     print(colored("\n\n Welcome To The ELB Menu ", 'green',attrs=['reverse', 'blink']))
+     print(colored("\n\n Welcome To The Utility Menu ", 'green',attrs=['reverse', 'blink']))
      print('==================================================================')
      print('|  1: Generate traffic to ELB                                      |')
      print('|  2: View the access logs for the instances                       |')
@@ -42,6 +48,7 @@ def elb_options():
      if choice == '3': copy_access_logs_to_local()
      if choice == '0': mainOptions()   
 
+#autoscaling menu
 def autoscaleOptions():
  choice = None
  while choice != '0':
@@ -49,13 +56,19 @@ def autoscaleOptions():
      print('==================================================================')
      print('|  1: Create new policies for my autoscale group                   |')
      print('|  2: Create new cloud watch alarm                                 |')
+     print(colored('|  3: Trigger an autoscaling scale up event based on new policy    |', 'green'))
      print(colored('|  0: Return to Main Menu                                                       |', 'red'))
      print('==================================================================')
      choice = input('which task do you want to perform? \n\n ')
      if choice == '1': policy_Options()
      if choice == '2': cloud_watch_options()
+     if choice == '3': 
+         if scaling_up_alarm_created:
+             trigger_autoscaling_event()  
+         else: print(colored('Please create a scaling up alarm and policy first', 'red')) 
      if choice == '0': mainOptions()   
 
+#policy menu
 def policy_Options():
  choice = None
  open_autoscale_conn()
@@ -63,7 +76,7 @@ def policy_Options():
      print('==================================================================')
      print(colored('|  1: Create a scaling up autoscale policy                         |', 'green'))
      print(colored('|  2: Create a scaling down autoscale policy                       |', 'green'))
-     print(colored('|  0: Return to Autoscale options                          |', 'red'))
+     print(colored('|  0: Return to Autoscale menu                          |', 'red'))
      print('==================================================================')
      choice = input('which task do you want to perform? \n\n ')
      if choice == '1':
@@ -74,6 +87,7 @@ def policy_Options():
          scaling_down_policy_created = create_scale_downPolicy()
      if choice == '0': autoscaleOptions()   
 
+#cloud watch menu
 def cloud_watch_options():
  set_up_cloud_watch()
  choice = None
@@ -81,23 +95,24 @@ def cloud_watch_options():
      print('==================================================================')
      print(colored('|  1: Create a scaling up cloud watch alarm                       |', 'green'))
      print(colored('|  2: Create a scaling down cloud watch alarm                     |', 'green'))
-     print(colored('|  0: Return to Autoscale options                         |', 'red'))
+     print(colored('|  0: Return to Autoscale menu                         |', 'red'))
      print('==================================================================')
      choice = input('which task do you want to perform? \n\n ')
      if choice == '1': 
          if scaling_up_policy_created:
-             create_scale_up_alarm()
+             global scaling_up_alarm_created
+             scaling_up_alarm_created = create_scale_up_alarm()
          else: print(colored('Please create a scale up policy first', 'red'))
      if choice == '2': 
          if scaling_down_policy_created:
-             create_scale_down_alarm()
+             global scaling_down_alarm_created
+             scaling_down_alarm_created = create_scale_down_alarm()
          else: print(colored('Please create a scale down policy first', 'red'))
      if choice == '0': autoscaleOptions()   
 
 def main():
  global connection
  connection = connect()
- getAllMyInstances(connection)#gather my instances and store them in an array
  mainOptions()
 
 if __name__ == '__main__':
